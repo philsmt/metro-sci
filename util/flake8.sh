f21815ae1ca52c6c7aa792d013cb784872e221df
Philipp Schmidt <philipp.schmidt@xfel.eu>
2020-07-13 11:51:52 +0200

    Fix flake8 util script

--- util/flake8.sh
+++ util/flake8.sh
@@ -1,2 +1,2 @@
-flake8 ../src/metro --exclude=../src/metro/external --ignore=W605,E226,W503,W504,E241
-flake8 setup.py
+flake8 src/metro --exclude=src/metro/external --ignore=W605,E226,W503,W504,E241,E402
+flake8 setup.py --ignore=W605,E226,W503,W504,E241,E402
