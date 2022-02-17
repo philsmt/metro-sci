fd39629b6897d8d5d1138b85d56df6d0e94f059c
Lutz Marder <lutz.marder@gmail.com>
2022-02-17 18:13:39 +0100

    Change design of log window using HTML style and include logging into file (metro.log)

--- src/metro/services/logger.py
+++ src/metro/services/logger.py
@@ -5,11 +5,13 @@
 
 
 import logging
+from os.path import join
 
 import metro
 
 QtCore = metro.QtCore
 QtWidgets = metro.QtWidgets
+QtGui = metro.QtGui
 
 # Configure level from which to show logged events: DEBUG, INFO, WARN, ERROR
 level = logging.INFO
@@ -21,17 +23,29 @@ level = logging.INFO
 base_handler = None
 # List of all non-local loggers created so far
 loggers = []
+txt_formatter = logging.Formatter('%(asctime)s [%(name)s] '
+                                  '%(levelname)s:\n%(message)s')
+
+# Create file handler to store the base log on the disk
+logfile_path = join(metro.LOCAL_PATH, 'metro.log')
+file_handler = logging.FileHandler(logfile_path)
+file_handler.setLevel(level)
+file_handler.setFormatter(txt_formatter)
 
 
 def log(name, local=False):
     log = logging.getLogger(name)
+
     if local:
         return log
+
+    log.addHandler(file_handler)
 
     global base_handler, loggers
     loggers.append(name)
     if base_handler is not None:
         base_handler.addLogger(name)
+
     return log
 
 
@@ -40,16 +54,23 @@ class QTextEditLogger(logging.Handler, QtCore.QObject):
     appendEntry = metro.QSignal(str)
 
     def __init__(self, parent, logger=None, base=False,
-                 format_str='%(asctime)s - %(name)s- '
-                            '%(levelname)s - %(message)s'):
+                 format_str=None, html=False):
         super().__init__()
         QtCore.QObject.__init__(self)
 
         self.widget = QtWidgets.QPlainTextEdit(parent)
         self.widget.setReadOnly(True)
-        self.appendEntry.connect(self.widget.appendPlainText)
 
-        self.setFormatter(logging.Formatter(format_str))
+        if html:
+            self.appendEntry.connect(self.widget.appendHtml)
+        else:
+            self.appendEntry.connect(self.widget.appendPlainText)
+
+        if format_str is None:
+            global txt_formatter
+            self.setFormatter(txt_formatter)
+        else:
+            self.setFormatter(logging.Formatter(format_str))
 
         if logger is not None:
             self.addLogger(logger)
@@ -63,6 +84,8 @@ class QTextEditLogger(logging.Handler, QtCore.QObject):
         entry = self.format(record)
         self.appendEntry.emit(entry)
         self.newEntry.emit()
+        # scrollbar = self.widget.verticalScrollBar()
+        # scrollbar.setValue(scrollbar.maximum())
 
     def addLogger(self, logger):
         if type(logger) is list:
@@ -76,14 +99,15 @@ class LogWindow(QtWidgets.QWidget):
     def __init__(self, parent=None, logger=None, base=False):
         super().__init__(parent)
 
-        self.logTextBox = QTextEditLogger(self, logger, base)
+        self.logTextBox = QTextEditLogger(self, logger, base, html=True)
+        self.logTextBox.setFormatter(HtmlFormatter())
 
         self.buttonClear = QtWidgets.QPushButton(self)
         self.buttonClear.setText('Clear log')
         self.buttonClear.setMaximumWidth(100)
 
         layout = QtWidgets.QVBoxLayout()
-        self.resize(QtCore.QSize(500, 300))
+        self.resize(QtCore.QSize(600, 400))
         self.setMinimumWidth(250)
         layout.addWidget(self.logTextBox.widget)
         layout.addWidget(self.buttonClear)
@@ -99,6 +123,37 @@ class LogWindow(QtWidgets.QWidget):
 
     def onNewEntry(self, slot):
         self.logTextBox.newEntry.connect(slot)
+
+
+class HtmlFormatter(logging.Formatter):
+    FORMATS = {
+        logging.DEBUG:
+            ("%(asctime)s [%(name)s] %(levelname)-8s [%(filename)s:%(lineno)d]"
+            " \n%(message)s",
+            QtGui.QColor(30, 120, 30)),
+        logging.INFO:
+            ("%(asctime)s [%(name)s] %(levelname)-8s\n%(message)s",
+            QtGui.QColor(40, 40, 200)),
+        logging.WARNING:
+            ('%(asctime)s [%(name)s] %(levelname)-8s\n%(message)s',
+            QtGui.QColor(200, 100, 20)),
+        logging.ERROR:
+            ("%(asctime)s [%(name)s] %(levelname)-8s\n%(message)s",
+            QtGui.QColor(200, 30, 30)),
+        }
+
+    FORMAT_STR = '<font color="{color}"><pre>{fmt}</pre></font>'
+
+    def format(self, record):
+        last_fmt = self._style._fmt
+        opt = self.FORMATS.get(record.levelno)
+        if opt:
+            fmt, color = opt
+            self._style._fmt = self.FORMAT_STR.format(
+                color=QtGui.QColor(color).name(), fmt=fmt.replace("\n", "<br>"))
+        res = logging.Formatter.format(self, record)
+        self._style._fmt = last_fmt
+        return res
 
 
 # Set the basic level of the root logger so all created loggers from now on
